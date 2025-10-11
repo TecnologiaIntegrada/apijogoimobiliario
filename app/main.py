b474de9f28892e301ddb299de0a4f848dccb099f
API Simulator <dev@tecnologiaintegrada.com>
2025-10-11 14:39:04 -0300

    docs: Adiciona referência ao teste técnico 7commAPI Python na descrição da API

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ app = FastAPI(
     title="API Simulador de Jogo Imobiliário",
     description="""
     API que simula partidas de um jogo similar ao Banco Imobiliário.
+    Teste técnico 7commAPI Python.
     
     ## Características
     
